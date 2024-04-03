9cea24c55e6918d6c0b234cee9992fa4145e0598
Renan Venturini <renanventurinioficial@gmail.com>
2024-04-03 01:32:16 -0300

    classe de contexto

---
+++ Teste_Operacoes_Matematicas/src/service/OperacoesMatematicasAPI/Data/OperacoesMatematicasContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OperacoesMatematicasAPI.Data.Table;
+using System.Reflection;
+
+namespace OperacoesMatematicasAPI.Data
+{
+    public class OperacoesMatematicasContext : DbContext
+    {
+        public OperacoesMatematicasContext(DbContextOptions<OperacoesMatematicasContext> options) : base(options) { }
+
+        public DbSet<TbUsuario> Usuario { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
